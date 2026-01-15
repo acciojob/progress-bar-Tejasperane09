d017896df9dee443d8bb8860e8ce93498df113f2
Tejasperane09 <89592090+Tejasperane09@users.noreply.github.com>
2026-01-15 10:43:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+// Next button
+next.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  update();
+});
+
+// Previous button
+prev.addEventListener("click", () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update();
+});
+
+// Update UI
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const activeCircles = document.querySelectorAll(".circle.active");
+
+  progress.style.width =
+    ((activeCircles.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  // Button state handling
+  prev.disabled = currentActive === 1;
+  next.disabled = currentActive === circles.length;
+}
